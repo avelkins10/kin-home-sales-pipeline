222a03f9fc2a11e5fb402e1f191183edf5f0487c
Austin Elkins <elkinsvaustin@gmail.com>
2025-10-30 12:40:03 -0600

    chore: Export new RepCard components from analytics index

--- components/analytics/index.ts
+++ components/analytics/index.ts
@@ -1,5 +1,7 @@
 export { default as ConfigurableLeaderboard, ConfigurableLeaderboardSkeleton } from './ConfigurableLeaderboard';
 export { CanvassingOverviewCard } from './CanvassingOverviewCard';
+export { RepCardOverviewCard } from './RepCardOverviewCard';
+export { RepCardQualityMetricsCard } from './RepCardQualityMetricsCard';
 export { CloserPerformanceTable } from './CloserPerformanceTable';
 export { DoorsKnockedTrendsCard } from './DoorsKnockedTrendsCard';
 export { LeadQualityAnalysisCard } from './LeadQualityAnalysisCard';
